e6c9dcd92109ddaa778de95fc29f7590861cfa2f
Matteo Meneguzzi <matteo.dev.meneguzzi@gmail.com>
2025-08-29 16:59:08 +0200

    app: trying @ts-ignore

--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@ import { log, chats } from './chats.js';
 const button = document.getElementById('enable-button')!;
 
 button.addEventListener('click', () => {
+  // @ts-ignore
   window.supportAI.enableAutoReply();
 });
 
